01cdf199eb592aa4a8f3bb7363dd9703eb286347
System Void <void@sysvoid.co>
2016-09-06 02:40:02 +0100

    Some UI changes and a better README.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,69 @@
-@extends('layouts.master')
+@extends('layouts.user')
 
 @section('content')
-    <h1>Welcome!</h1>
-    <p>This is neat.</p>
+    <div class="row">
+        <div class="col-xs-6">
+            <div class="module">
+                <div class="heading">My Category</div>
+                <div class="body spaced">
+                    <p><a href="#">Setting up your ZeroIssues knowledgebase</a></p>
+                    <p><a href="#">How to create new tickets</a></p>
+                    <p><a href="#">How to add/remove users to/from support departments</a></p>
+
+                    <div class="button margin-top-50">
+                        <a href="#" class="button">View more...</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xs-6">
+            <div class="module">
+                <div class="heading">My Second Category</div>
+                <div class="body spaced">
+                    <p><a href="#">Setting up your ZeroIssues knowledgebase</a></p>
+                    <p><a href="#">How to create new tickets</a></p>
+
+                    <div class="button margin-top-50">
+                        <a href="#" class="button">View more...</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row margin-top-50">
+        <div class="col-xs-6">
+            <div class="module">
+                <div class="heading">My Third Category</div>
+                <div class="body spaced">
+                    <p><a href="#">Setting up your ZeroIssues knowledgebase</a></p>
+                    <p><a href="#">Setting up your ZeroIssues knowledgebase</a></p>
+                    <p><a href="#">Setting up your ZeroIssues knowledgebase</a></p>
+                    <p><a href="#">How to create new tickets</a></p>
+                    <p><a href="#">How to add/remove users to/from support departments</a></p>
+
+                    <div class="button margin-top-50">
+                        <a href="#" class="button">View more...</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xs-6">
+            <div class="module">
+                <div class="heading">My Fourth Category</div>
+                <div class="body spaced">
+                    <p><a href="#">Setting up your ZeroIssues knowledgebase</a></p>
+                    <p><a href="#">Setting up your ZeroIssues knowledgebase</a></p>
+                    <p><a href="#">Setting up your ZeroIssues knowledgebase</a></p>
+                    <p><a href="#">How to add/remove users to/from support departments</a></p>
+
+                    <div class="button margin-top-50">
+                        <a href="#" class="button">View more...</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
---
+++ resources/views/layouts/user.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+
+        <title>{{ (empty($title) ? "" : $title . " | ") . config('app.name') }}</title>
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,500,600">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <nav>
+            @include('layouts.partials.navigation')
+        </nav>
+
+        <div class="container">
+            @if (!isset($disableSearch) || !$disableSearch)
+                <form action="" method="get">
+                    <input type="text" name="q" placeholder="Search our knowledgebase..." autofocus>
+                </form>
+            @endif
+
+            @if (!isset($disableSidebar) || !$disableSidebar)
+                <div class="row">
+                    <div class="col-xs-8">
+                        @yield('content')
+                    </div>
+
+                    <div class="col-xs-4">
+                        <div class="sidebar">
+                            @include('layouts.partials.sidebar')
+                        </div>
+                    </div>
+                </div>
+            @else
+                @yield('content')
+            @endif
+        </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.26/vue.min.js"></script>
+        @include('layouts.partials.pusher')
+    </body>
+</html>
